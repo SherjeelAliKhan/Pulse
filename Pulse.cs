fb495da60810ce0261249dd5ca7aea0afb5cc4e7
SherjeelAlikhan\Sherjeel <sherjeel.sak@gmail.com>
2017-12-10 18:03:35 +0500

    Created Class

---
+++ Pulse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulse : MonoBehaviour {
+
+
+	float rate = 0.5f;
+	float maxScale = 1.2f;
+	float minScale = 0.8f;
+
+	void Update () {
+		float scale = (Mathf.Sin(Time.time * (rate * 2 * Mathf.PI)) + 1) / 2;
+		scale =  Mathf.Lerp (minScale, maxScale, scale);
+		transform.localScale = new Vector3 (scale,scale,scale);
+	}
+}
